2cfb676da14ec75b79e606b9ddebc616424013a5
Gaurav kumar <indiagauravkumar@gmail.com>
2025-01-20 22:28:24 +0530

    configure image domains in Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //here write the config for all kind of image source from the internet.
+  images: {
+    domains: ['t4.ftcdn.net'],
+  },
 };
 
 export default nextConfig;
